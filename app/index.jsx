d29c6f1a1771fed9645379c6bbc5ec23457f2700
chanesarcon <sarconchane@gmail.com>
2025-08-27 15:39:54 +0800

    firebase Configuration

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        LLA  G O A L S
+        LLA  G O A L TRACKER
       </Text>
       <Link style={styles.link} href="/goals">
         View Your Goals
