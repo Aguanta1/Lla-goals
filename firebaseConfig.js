7c7227d68ceabc2f9f3571d63f17f0a86ca2b9e6
chanesarcon <sarconchane@gmail.com>
2025-08-27 15:34:43 +0800

    firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAKj3KdYI-EVeQ49eL9sDjaqC-ChbS5y6w",
+  authDomain: "lla-goal.firebaseapp.com",
+  projectId: "lla-goal",
+  storageBucket: "lla-goal.firebasestorage.app",
+  messagingSenderId: "759639730484",
+  appId: "1:759639730484:web:9ed52629e03ea068795d61"
+};
+
+
+const app = initializeApp(firebaseConfig);
